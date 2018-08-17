e064ae7b8ffc813c03fb956233c7b2d51132d57b
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-08-17 15:21:53 -0300

    Base broken application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,24 @@
 # Base helper
 
 module ApplicationHelper
+  def app_name
+    Rails.configuration.x['brand']['name']
+  end
+
+  def app_url
+    Rails.configuration.x['brand']['url']
+  end
+
+  def contact_email
+    Rails.configuration.x['brand']['contact']
+  end
+
+  def brand_tag
+    image_tag "#{Rails.configuration.x['core_engine']}/app.svg", alt: app_name
+  end
+
+  # Each page that wants a title should provide it
+  def page_title
+    content_for :page_title
+  end
 end
---
+++ config/initializers/sisinta.rb
@@ -0,0 +1,3 @@
+# Custom application configuration loaded from config/sisinta.yml per
+# environment
+Rails.application.config.x = Rails.application.config_for(:sisinta)
