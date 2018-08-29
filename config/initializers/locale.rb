c35b4df1d13bc2e6e471f58855d267f1b91d2fd3
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-08-29 03:07:54 -0300

    Select locale for requests by url

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,5 +1,5 @@
 # Whitelist locales available for the application
-I18n.available_locales = [:es]
+I18n.available_locales = [:es, :en]
  
 # Set default locale to something other than :en
 I18n.default_locale = :es
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
+# Application routes
 Rails.application.routes.draw do
-  # Default map as landing page.
-  root to: 'map#index'
-  get 'map/index'
+  # Route the whole app with an optional locale. If it's missing the controller
+  # should try to get it from current user o use the default in case it's
+  # missing. Only allows configured locales, the rest returns 404
+  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    # Default map as landing page.
+    root to: 'map#index'
+    get 'map/index'
 
-  # User management engine with sessions, passwords, registrations and
-  # confirmations
-  devise_for :users
+    # User management engine with sessions, passwords, registrations and
+    # confirmations
+    devise_for :users
 
-  # Administration Panel
-  ActiveAdmin.routes(self)
+    # Administration Panel
+    ActiveAdmin.routes(self)
 
-  resources :profiles, only: [:index, :show]
+    resources :profiles, only: [:index, :show]
+  end
 end
