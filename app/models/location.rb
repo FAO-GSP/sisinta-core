1dc90f56f5e3cd240138687173677914fe39d7a6
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-09-08 03:05:43 -0300

    Create coordinates from [latitude, longitude] values

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,10 +1,30 @@
-# Profile location data, notably coordinates
+# Profile location data, notably [latitude, longitude] coordinates
 class Location < ApplicationRecord
   belongs_to :profile
 
+  attr_accessor :latitude, :longitude
+
   validates :profile, presence: true, uniqueness: true
+
+  after_initialize :load_latitude_and_longitude
+  before_save :update_coordinates
 
   def self.factory
     RGeo::ActiveRecord::SpatialFactoryStore.instance.default
+  end
+
+  private
+
+  # Updates the geolocation column with provided [longitude, latitude] values
+  def update_coordinates
+    if longitude.present? && latitude.present?
+      self.coordinates = Location.factory.point longitude, latitude
+    end
+  end
+
+  # Loads virtual attributes from the DB values
+  def load_latitude_and_longitude
+    self.latitude = coordinates.try :latitude
+    self.longitude = coordinates.try :longitude
   end
 end
--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 describe Location do
+  subject { create :location }
+
   describe 'validations' do
     let(:profile) { create :profile }
 
@@ -13,6 +15,36 @@ describe Location do
       existing = create(:location)
 
       build(:location, profile: existing.profile).wont_be :valid?
+    end
+  end
+
+  describe '#coordinates' do
+    it 'can handle latitude' do
+      subject.latitude = 10
+      subject.latitude.must_equal 10
+    end
+
+    it 'can handle longitude' do
+      subject.longitude = 10
+      subject.longitude.must_equal 10
+    end
+
+    it 'creates them with latitude and longitude separately' do
+      location = create :location, latitude: 15, longitude: 30
+
+      location.coordinates.must_be :present?
+      location.coordinates.latitude.must_equal 15
+      location.coordinates.longitude.must_equal 30
+    end
+
+    it 'can access latitude and longitude directly' do
+      location = create :location, :geolocated
+
+      # reload doesn't trigger after_initialize
+      location = Location.find(location.id)
+
+      location.latitude.must_equal location.coordinates.latitude
+      location.longitude.must_equal location.coordinates.longitude
     end
   end
 end
