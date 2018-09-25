8aa1deb6f3d754667685738fd391b597588c1d51
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-09-25 03:16:38 -0300

    Don't load Google Maps on system tests

--- app/helpers/map_helper.rb
+++ app/helpers/map_helper.rb
@@ -1,17 +1,18 @@
 # Maps related helpers
 
 module MapHelper
-  # For inserting only in pages with maps
-  def google_maps_js_tag
-    # Don't reload Google Maps API when visiting with turbolinks
-    javascript_include_tag google_maps_api_url, data: { turbolinks_eval: false }
-  end
-
   # The current API URL with client's key
-  def google_maps_api_url
+  def self.google_maps_api_url
     [
       'http://maps.google.com/maps/api/js?v=3&libraries=geometry&key=',
       Rails.configuration.x['google_api_key']
     ].join.html_safe
+  end
+
+  # For inserting only in pages with maps
+  def google_maps_js_tag
+    javascript_include_tag MapHelper.google_maps_api_url,
+      # Don't reload Google Maps API when visiting with turbolinks
+      data: { turbolinks_eval: false }
   end
 end
