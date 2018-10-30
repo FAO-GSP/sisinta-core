ff44ef88b736d7b9b0f48266fdf376be8dcd3067
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-10-29 23:01:41 -0300

    Configure SMTP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,18 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # Configure SMTP with sendgrid
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true,
+    user_name: ENV['SISINTA_SMTP_USERNAME'],
+    password: ENV['SISINTA_SMTP_PASSWORD'],
+    domain: ENV['SISINTA_SMTP_DOMAIN']
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
