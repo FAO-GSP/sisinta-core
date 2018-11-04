b118fed14f3b7e2c8a40cb32c74fcc273d87cdb1
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2018-11-04 07:07:36 -0300

    Start puma on boot

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,13 +1,17 @@
 # Cron jobs for the application.
+# TODO Extract shared path from ENV
 
 # Use its own log.
 set :output, '/srv/http/sislac/shared/log/cron.log'
 
-# Custom job type with ENV variables set
-job_type :env_command, 'cd :path && RAILS_ENV=:environment :task'
+# Custom job type with ENV variables set.
+job_type :env_command, 'cd :path && RAILS_ENV=:environment bin/:task :output'
 
 # Start services on boot.
 every :reboot do
   # delayed_job.
-  env_command 'bin/delayed_job -n 2 restart'
+  env_command 'delayed_job -n 2 restart'
+
+  # puma.
+  env_command 'puma -C /srv/http/sislac/shared/puma.rb --daemon'
 end
