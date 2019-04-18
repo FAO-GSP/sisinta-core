4a140f60229d3e8bf8d8b807856c1713bb096644
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2019-04-18 01:42:15 -0300

    Tabs in Imports#new with metadata

--- app/helpers/imports_helper.rb
+++ app/helpers/imports_helper.rb
@@ -5,12 +5,17 @@ module ImportsHelper
   def select_metadata_tag(field)
     select_tag 'import[metadata][]',
       options_from_collection_for_select(MetadataType.for(field), :id, :value, selected_for(field)),
-      id: "metadata_#{field}", class: 'form-control'
+      id: "metadata_#{field}", class: 'form-control', include_blank: true
   end
 
   # Deduce which id from the metadata array is relevant for the provided field.
   def selected_for(field)
     (import.metadata.map(&:to_i) & MetadataType.for(field).ids).first
+  end
+
+  # Format the list of allowed country codes as options for select.
+  def country_codes(selected = nil)
+    options_for_select Rails.configuration.engine.default_country_codes, selected
   end
 
   private
