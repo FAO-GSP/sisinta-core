f805bca716a796aa4372d2c1850f7cd5c7b6491c
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2019-06-22 08:59:14 -0300

    Streamline profiles admin UI

--- app/admin/profiles.rb
+++ app/admin/profiles.rb
@@ -18,7 +18,7 @@ ActiveAdmin.register Profile do
     :contact, :type_id, :license_id, :country_code
 
   # Don't load every association on index
-  remove_filter :user, :location, :layers, :metadata_entries
+  remove_filter :user, :location, :layers, :metadata_entries, :metadata_types
 
   decorate_with ProfileDecorator
 
@@ -27,6 +27,25 @@ ActiveAdmin.register Profile do
       li link_to "#{Layer.model_name.human(count: 2)} (#{resource.layers.count})", admin_profile_layers_path(resource)
       li link_to "#{MetadataType.model_name.human(count: 2)} (#{resource.metadata_entries.count})", admin_profile_metadata_entries_path(resource)
     end
+  end
+
+  show do |profile|
+    attributes_table do
+      row :identifier
+      row :country_code
+      row :user
+      row :contact
+      row :source
+      row :type
+      row :order
+      row :date
+      row :license
+      row :public
+      row :created_at
+      row :updated_at
+    end
+
+    active_admin_comments
   end
 
   index do
